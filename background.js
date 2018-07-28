27e9a5005258629ef94ac07d702a51f7c182a9d9
0x401 <0x401@users.noreply.github.com>
2018-07-28 14:23:14 +0800

    1

---
+++ background.js
@@ -0,0 +1,28 @@
+var domains={};
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
+	if(changeInfo.status=="loading") domains[tabId]=[] ;
+	});
+
+function add_domain(tabId,domain){
+	if(domains[tabId]){
+		if(!domains[tabId].includes(domain)){
+			domains[tabId].unshift(domain);
+			}
+	}else{
+		console.log("++"+tabId);
+		domains[tabId] =[];
+		add_domain(tabId,domain);
+	}	
+}
+chrome.webRequest.onResponseStarted.addListener(function(info){
+	for(let key in info){
+		let domain =info.url.split("/")[2];
+		add_domain(info.tabId,domain);
+		chrome.browserAction.setBadgeText({
+			text: domains[info.tabId].length.toString(),tabId:info.tabId
+		});
+	}
+},
+{urls: ["<all_urls>"]},
+[]
+);
---
+++ popup.js
@@ -0,0 +1,15 @@
+﻿let bg=chrome.extension.getBackgroundPage();
+let domains=bg.domains;
+
+chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+  let currentTab = tabs[0]; 
+  let current_domains = domains[currentTab.id];
+  let domain_list=document.querySelector("#domain_list");
+  for(let key in current_domains){
+	let li = document.createElement("li");
+	console.log("!"+current_domains[key]);
+	li.innerText = current_domains[key];
+	console.log(li);
+	domain_list.appendChild(li);
+  }
+});
