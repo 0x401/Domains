13cf73be4078d266cd8edbfbec28664cee8388b5
0x401 <0x401@users.noreply.github.com>
2018-08-03 15:40:30 +0800

    忽略内部网页

--- background.js
+++ background.js
@@ -1,22 +1,21 @@
 var domains={};
+let fiter=["chrome:","chrome-extension:"];
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
 	if(changeInfo.status=="loading") domains[tabId]=[] ;
 	});
-
 function add_domain(tabId,domain){
 	if(domains[tabId]){
 		if(!domains[tabId].includes(domain)){
 			domains[tabId].unshift(domain);
 			}
 	}else{
-		console.log("++"+tabId);
 		domains[tabId] =[];
 		add_domain(tabId,domain);
 	}	
 }
 chrome.webRequest.onResponseStarted.addListener(function(info){
 	for(let key in info){
-		let domain =info.url.split("/")[2];
+		let domain = info.url.split("/")[2];
 		if(info.tabId!=-1){
 		add_domain(info.tabId,domain);
 		chrome.browserAction.setBadgeText({
